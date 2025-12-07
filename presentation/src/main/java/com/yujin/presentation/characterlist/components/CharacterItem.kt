23abacf91ad529cabd683bbe97fa80414d317279
Yu Jin <qldls0307@naver.com>
2025-12-07 16:56:54 +0900

    chore: Item 속 사진 테두리 둥글게

--- presentation/src/main/java/com/yujin/presentation/characterlist/components/CharacterItem.kt
+++ presentation/src/main/java/com/yujin/presentation/characterlist/components/CharacterItem.kt
@@ -9,11 +9,13 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
@@ -39,11 +41,13 @@ fun CharacterItem(
                 .padding(12.dp),
             verticalAlignment = Alignment.CenterVertically
         ) {
+
             AsyncImage(
                 model = character.image,
                 contentDescription = character.name,
                 modifier = Modifier
-                    .size(80.dp),
+                    .size(80.dp)
+                    .clip(RoundedCornerShape(8.dp)),
                 contentScale = ContentScale.Crop
             )
 
